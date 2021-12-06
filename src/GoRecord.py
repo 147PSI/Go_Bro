445f65cc1820a14fca7932af80ddb300f67ffa8d
x.huang <huangx66@mcmaster.ca>
2021-12-06 01:51:03 -0500

    added game interface and rest controller

--- src/GoRecord.py
+++ src/GoRecord.py
@@ -3,9 +3,9 @@ from datetime import datetime
 import os
 
 
-def set_new_node(sgf_game, color, position):
+def set_new_node(sgf_game, color, row, col):
     new_node = sgf_game.extend_main_sequence()
-    new_node.set(color, position)
+    new_node.set(color, (row, col))
 
     return sgf_game.serialise().decode("UTF-8")
 
@@ -22,20 +22,22 @@ class GoRecord():
                 return True
         return False
 
-    def write_new_step_to_sgf_file(self, color, position):
+    def write_new_step_to_sgf_file(self, color, row, col):
         if self._check_sgf_file_existence():
             with open(self.record_storage_path + self.sessionId + ".sfg", "r") as f:
                 data = f.read()
 
                 g = sgf.Sgf_game.from_string(data)
-                sgf_string = set_new_node(g, color, position)
+                sgf_string = set_new_node(g, color, row, col)
 
         else:
             g = sgf.Sgf_game(13)
-            sgf_string = set_new_node(g, color, position)
+            sgf_string = set_new_node(g, color, row, col)
 
         with open(self.record_storage_path + self.sessionId + ".sfg", "w") as f:
             f.write(sgf_string)
+
+        return self.get_game_sequence()
 
     def get_game_sequence(self):
         if self._check_sgf_file_existence():
@@ -47,30 +49,35 @@ class GoRecord():
                 sgf_list = sgf_string.split(";")
                 # properties = sgf_list[1]
                 game_sequence = str(sgf_list[2:])
-
-                return game_sequence
+        else:
+            g = sgf.Sgf_game(13)
+            sgf_string = g.serialise().decode("UTF-8")
+            with open(self.record_storage_path + self.sessionId + ".sfg", "w") as f:
+                f.write(sgf_string)
+            game_sequence = "New game session has been created!"
+        return game_sequence
 
 
 if __name__ == "__main__":
     go_game = GoRecord()
-    go_game.write_new_step_to_sgf_file("W", (1, 4))
-    go_game.write_new_step_to_sgf_file("B", (2, 6))
-    go_game.write_new_step_to_sgf_file("W", (3, 6))
-    go_game.write_new_step_to_sgf_file("B", (2, 8))
+    go_game.write_new_step_to_sgf_file("W", 1, 4)
+    go_game.write_new_step_to_sgf_file("B", 2, 6)
+    go_game.write_new_step_to_sgf_file("W", 3, 6)
+    go_game.write_new_step_to_sgf_file("B", 2, 8)
 
     sessionId = go_game.sessionId
 
     print(go_game.get_game_sequence())
 
     go_game2 = GoRecord()
-    go_game2.write_new_step_to_sgf_file("W", (9, 3))
-    go_game2.write_new_step_to_sgf_file("B", (6, 5))
+    go_game2.write_new_step_to_sgf_file("W", 9, 3)
+    go_game2.write_new_step_to_sgf_file("B", 6, 5)
     sessionId2 = go_game2.sessionId
 
     print(go_game2.get_game_sequence())
 
     go_game3 = GoRecord(sessionId=sessionId)
-    go_game3.write_new_step_to_sgf_file("W", (7, 2))
-    go_game3.write_new_step_to_sgf_file("B", (6, 12))
+    go_game3.write_new_step_to_sgf_file("W", 7, 2)
+    go_game3.write_new_step_to_sgf_file("B", 6, 12)
 
     print(go_game3.get_game_sequence())
---
+++ src/GoServer.py
@@ -0,0 +1,48 @@
+from GoRecord import GoRecord
+from flask_restful import Resource, Api
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Play(Resource):
+    def get(self, sessionId):
+        go = GoRecord(sessionId=sessionId)
+        return {sessionId: go.get_game_sequence()}
+
+    def put(self, sessionId):
+        go = GoRecord(sessionId=sessionId)
+        response = go.write_new_step_to_sgf_file(request.form['color'],
+                                                 int(request.form['row']),
+                                                 int(request.form['col']))
+        return {sessionId: response}
+
+    def delete(self, sessionId):
+        for file in os.listdir("./"):
+            if file.endswith('sfg') and file.split(".")[0] == sessionId:
+                os.remove("./"+sessionId+".sfg")
+
+
+class History(Resource):
+    def get(self):
+        existing_game_session = {}
+
+        for file in os.listdir("./"):
+            if file.endswith('sfg'):
+                existing_game_session[str(len(existing_game_session))] = file.split(".")[0]
+        return {"Session Count": str(len(existing_game_session)),
+                "Existing Session": existing_game_session}
+
+    def delete(self):
+        for file in os.listdir("./"):
+            if file.endswith('sfg'):
+                os.remove("./"+file+".sfg")
+
+
+api.add_resource(Play, '/sessions/<string:sessionId>')
+api.add_resource(History, '/all_sessions')
+
+if __name__ == "__main__":
+    app.run()
